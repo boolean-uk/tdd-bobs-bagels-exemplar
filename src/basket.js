801a3721e103395fa56668469e5f20ef21273a75
phillipdunn <phillipdunn@hotmail.com>
2022-01-13 18:07:56 +0000

    Add - check basket logic

---
+++ src/basket.js
@@ -0,0 +1,13 @@
+/* eslint-disable semi */
+/* eslint-disable space-before-function-paren */
+class Basket {
+  constructor() {
+    this.basket = [];
+  }
+
+  checkBasket() {
+    return this.basket;
+  }
+}
+
+module.exports = Basket;
